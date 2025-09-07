a0eb8387056ff1fb32438df5ba0e4c93a27358da
maxim <maxim228338@mail.ru>
2025-09-07 18:35:27 +0300

    4

--- js/main.js
+++ js/main.js
@@ -4,30 +4,25 @@ let app = new Vue({
        product: "Socks",
        image: "./assets/vmSocks-green-onWhite.jpg",
        altText: "A pair of socks",
-       inStock: true,
-       cart: 0,  
+       inStock: false,
        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
        variants: [
            {
                variantId: 2234,
                variantColor: 'green',
-               variantImage: "./assets/vmSocks-green-onWhite.jpg" 
+               variantImage: "./assets/vmSocks-green-onWhite.jpg",
            },
            {
                variantId: 2235,
                variantColor: 'blue',
-               variantImage: "./assets/vmSocks-blue-onWhite.jpg" 
+               variantImage: "./assets/vmSocks-blue-onWhite.jpg",
            }
        ],
+       cart: 0
    },
-   methods: {  
+   methods: {
        addToCart() {
            this.cart += 1
-       },
-       removeFromCart() {
-           if (this.cart > 0) {
-               this.cart -= 1
-           }
        },
        updateProduct(variantImage) {
            this.image = variantImage
