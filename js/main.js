ace8075a645c06f84b5942db076f7f1a88688043
maxim <maxim228338@mail.ru>
2025-09-12 16:10:20 +0300

    5

--- js/main.js
+++ js/main.js
@@ -2,20 +2,24 @@ let app = new Vue({
    el: '#app',
    data: {
        product: "Socks",
+       brand: 'Vue Mastery',
        image: "./assets/vmSocks-green-onWhite.jpg",
        altText: "A pair of socks",
        inStock: false,
+       onSale: true,
        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
        variants: [
            {
                variantId: 2234,
                variantColor: 'green',
                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+               variantQuantity: 10
            },
            {
                variantId: 2235,
                variantColor: 'blue',
                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+               variantQuantity: 0
            }
        ],
        cart: 0
@@ -27,5 +31,27 @@ let app = new Vue({
        updateProduct(variantImage) {
            this.image = variantImage
        }
-   }
+   },
+
+   computed: {
+    title() {
+        return this.brand + ' ' + this.product;
+    },
+    image() {
+        return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+        return this.variants[this.selectedVariant].variantQuantity;
+    },
+    sale() {
+        if (this.onSale) {
+            return this.brand + ' ' + this.product + ' is on sale!';
+        } else {
+            return this.brand + ' ' + this.product + ' is not on sale';
+        }
+    }
+}
+
 })
+
+
